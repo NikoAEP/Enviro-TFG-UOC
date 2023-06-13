07054e1531ef21aadd343efbd11b2312313ee09a
NikoAEP <nesteche@uoc.edu>
2023-06-13 23:54:13 +0200

    Updated Tutorial

--- Enviro/Assets/Scripts/UI Scripts/InstructionPopUp.cs
+++ Enviro/Assets/Scripts/UI Scripts/InstructionPopUp.cs
@@ -1,20 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class InstructionPopUp : MonoBehaviour
 {
     public GameObject tutorialContainer; // objeto del contenedor de tutorial/instrucciones
-    public GameObject mainUI; // objeto del UI/HUD principal
-    public static bool isPaused; // variable de pausa
+    public GameObject[] HUDElements;
+    public GameObject tipPopups; // objeto de consejos
+    public Button skipBtn; // botón para saltar el tutorial y jugar
+    public Button nextBtn; // botón para pasar al siguiente tutorial
+
+    private int currentIndex = 0; // mantener control del índice actual de popups
 
     void Start()
     {
         tutorialContainer.SetActive(true); // por defecto se activa el tutorial
         Time.timeScale = 0f; // se para el tiempo
-        isPaused = true; // está pausado
-        mainUI.SetActive(false); // se desactiva el HUD 
+        HideAllPopUps();
+    }
+
+    public void FirstPopUp() // Enseña el primer tutorial
+    {
+        ShowTutorialPopUp();
+        nextBtn.gameObject.SetActive(false);
+    }
+
+    public void NextPopUp() // Enseña el siguiente tutorial
+    {
+        HideTutorialPopUp(); // Esconde el actual
+
+        currentIndex++; // pasa al siguiente índice
+        if (currentIndex < HUDElements.Length)
+        {
+            ShowTutorialPopUp(); // Enseña el actual
+        }
+        else
+        {
+            EndTutorial(); // Cuando llega al final acaba el tutorial
+        }
+    }
+
+    private void ShowTutorialPopUp() // Mostrar tutorial
+    {
+        // Activa el elemento del HUD actual
+        HUDElements[currentIndex].SetActive(true);
+
+        // Activa el botón de "Siguiente"
+        nextBtn.interactable = true;
+    }
+
+    private void HideTutorialPopUp() // Esconder tutorial
+    {
+        // Esconde el elemento del HUD actual
+        HUDElements[currentIndex].SetActive(false);
+
+        // Desactiva el botón de "Siguiente"
+        nextBtn.interactable = false;
+    }
+
+    private void HideAllPopUps()
+    {
+        // Esconde todo los elementos
+        foreach (var element in HUDElements)
+        {
+            element.SetActive(false);
+        }
+    }
+    
+    private void EndTutorial() // Acabar Tutorial
+    {
+        HideAllPopUps(); // esconde todos
+        nextBtn.interactable = false;
+        ResumeGame(); // Resume juego
     }
 
     public void ResumeGame()
@@ -22,6 +81,6 @@ public class InstructionPopUp : MonoBehaviour
         tutorialContainer.SetActive(false); // se desactiva el panel de pausa
         Time.timeScale = 1f; // se reanuda el tiempo
         PauseMenu.isPaused = false; // ya no está pausado
-        mainUI.SetActive(true); // se reaactiva el HUD 
+        tipPopups.SetActive(true); // se activan los consejos
     }
 }
